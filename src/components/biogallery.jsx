8231a3b45cf4b7cf8eaf2ee7eb8c74d84c2918c7
jathin12e <126076303+jathin12e@users.noreply.github.com>
2025-07-28 13:29:37 +0530

    changed

--- src/components/biogallery.jsx
+++ src/components/biogallery.jsx
@@ -13,7 +13,7 @@ const mediaList = [
      {type: "image", src:'/BioPonds/WhatsApp Image 2024-09-05 at 13.55.02 (1).jpeg'},
      
   { type: "video", src: "https://res.cloudinary.com/dfzliqupz/video/upload/v1753508152/WhatsApp_Video_2024-09-05_at_13.50.55_sxgjwk.mp4" },
-  { type: "video", src: "/BioPonds/WhatsApp Video 2024-03-05 at 18.24.50.mp4" },
+  { type: "video", src: "https://res.cloudinary.com/dfzliqupz/video/upload/v1753689206/WhatsApp_Video_2024-03-05_at_18.24.50_h1esoy.mp4" },
   { type: "video", src: "/BioPonds/bio pond.mp4" },
   {type:"video" , src:"https://res.cloudinary.com/dfzliqupz/video/upload/v1753508660/VID_20211229_115620_whbh8t.mp4"}
   // Add more media as needed
